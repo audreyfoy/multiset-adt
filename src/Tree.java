f9cd26ef1af81365b2fa492442f7d4dbe5799ebf
kate levey <katelevey@kates-MacBook-Pro.local>
2023-09-18 10:40:38 -0400

    Started on Tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Tree {
+
+    private Integer root;
+    private ArrayList<Tree> subtrees;
+
+    public Tree(int root) {
+        this.root = root;
+        this.subtrees = new ArrayList<Tree>();
+    }
+
+    public Tree(int root, ArrayList<Tree> subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public int __len__() {
+        if (this.isEmpty()) {
+            return 0;
+        } else {
+            int size = 1;
+            for (Tree subtree : this.subtrees) {
+                size += subtree.__len__();
+            }
+        }
+    }
 }
+
+
